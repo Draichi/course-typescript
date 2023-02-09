147089f633086d482a277672cfc127237c0c883c
@Draichi <lucasdraichi@gmail.com>
2023-02-09 12:55:35 -0300

    feat: create autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,31 @@
+/**
+ * Decorator used to automatically bind the function with the `this`
+ * @param _ target
+ * @param _2 methodName
+ * @param description the PropertyDescriptor
+ * @returns the adjusted method
+ */
+function autobind(_: any, _2: string, description: PropertyDescriptor) {
+  const originalMethod = description.value;
+  const adjustedMethod: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+
+      return boundFunction;
+    },
+  };
+
+  return adjustedMethod;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputelement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -15,8 +39,30 @@ class ProjectInput {
       true
     );
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = "user-input";
 
+    this.titleInputElement = this.formElement.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputelement = this.formElement.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  @autobind
+  private onSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.formElement.addEventListener("submit", this.onSubmit);
   }
 
   private attach() {
